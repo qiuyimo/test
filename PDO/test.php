2a982ec12b8263a2c105311337ae66d31e64f496
qiuyu <530004000@qq.com>
2018-02-12 18:11:07 +0800

    PDO, 预防sql注入的原理.

--- PDO/test.php
+++ PDO/test.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 测试 PDO.
+ * 测试 PDO, 了解 PDO 预防 SQL 注入的原理
  *
  * 防止sql 注入原理:
  * SQL是有语义结构，而语义结构是通过抽象语法树（AST）表达。
@@ -8,11 +8,25 @@
  * prepared statement相当于预先分析一遍其语义，把AST固定下来，用户输入参数后，
  * 参数只会替代AST中的参数节点，而不会转变成其他节点，因此就不会发生注入问题。
  *
+ * 嵌入式SQL:
+ * 是一种将SQL语句直接写入C语言等编程语言的源代码中的方法。
+ * 借此方法，应用程序可以拥有访问数据以及处理数据的能力。
+ * 在这一方法中，将SQL文嵌入的目标代码的语言称为宿主语言。
+ *
+ * 预编译:
+ * 有DBMS的预处理程序对源程序进行扫描，识别出SQL语句，把它们转换成主语言调用语句，
+ * 以使主语言编译程序能识别它，最后由主语言的编译程序将整个源程序编译成目标码。
+ *
+ * DBMS:
+ * Database Management System
+ *
  * @link https://www.zhihu.com/question/43581628
  * @link http://www.king-liu.net/?p=1006
  * @link http://wwxiong.com/hexo_blog/2017/06/27/2017-06-28-sql-optimization-up/
  * @link http://wwxiong.com/hexo_blog/2017/06/09/2017-06-10-php-sql-injection/
  * @link http://blog.jobbole.com/100349/
+ * @link http://blog.sina.com.cn/s/blog_c38ac7580101dfmm.html
+ * @link http://www.cnblogs.com/simpman/p/6510604.html
  *
  * User: qiuyu
  * Date: 2018/2/12
