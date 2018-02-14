35821c1ddaf14f990970bd06c5d93b51add3c768
qiuyu <530004000@qq.com>
2018-02-14 23:42:11 +0800

    monolog

--- PDO/test.php
+++ PDO/test.php
@@ -22,25 +22,34 @@
  *
  * @link https://www.zhihu.com/question/43581628
  * @link http://www.king-liu.net/?p=1006
- * @link http://wwxiong.com/hexo_blog/2017/06/27/2017-06-28-sql-optimization-up/
- * @link http://wwxiong.com/hexo_blog/2017/06/09/2017-06-10-php-sql-injection/
  * @link http://blog.jobbole.com/100349/
  * @link http://blog.sina.com.cn/s/blog_c38ac7580101dfmm.html
  * @link http://www.cnblogs.com/simpman/p/6510604.html
+ * @link https://stackoverflow.com/questions/134099/are-pdo-prepared-statements-sufficient-to-prevent-sql-injection
  *
  * User: qiuyu
  * Date: 2018/2/12
  * Time: 下午12:21
  */
 
+// 如果在DSN中指定了charset, 是否还需要执行set names <charset>呢？
+// 是的，不能省。set names <charset>其实有两个作用：
+// A.  告诉mysql server, 客户端（PHP程序）提交给它的编码是什么
+// B.  告诉mysql server, 客户端需要的结果的编码是什么
+// 也就是说，如果数据表使用gbk字符集，而PHP程序使用UTF-8编码，我们在执行查询前运行set names utf8, 告诉mysql server正确编码即可，无须在程序中编码转换。这样我们以utf-8编码提交查询到mysql server, 得到的结果也会是utf-8编码。省却了程序中的转换编码问题，不要有疑问，这样做不会产生乱码。
+
+// 在DSN中指定charset的作用是什么?
+// 只是告诉PDO, 本地驱动转义时使用指定的字符集（并不是设定mysql server通信字符集），设置mysql server通信字符集，还得使用set names <charset>指令。
+
 require dirname(__FILE__)."/../vendor/autoload.php";
 
-$db = new PDO("mysql:host=127.0.0.1;port=3306;dbname=new_spider;charset=utf8", "root", "root");
+$db = new PDO("mysql:host=127.0.0.1;port=3306;dbname=test;charset=utf8", "root", "root");
 
 // Return an array of available PDO drivers,If no drivers are available, it returns an empty array.
 dump($db->getAvailableDrivers());
 
-$statement = $db->query("select * from store limit 1");
+// query runs a standard SQL statement and requires you to properly escape all data to avoid SQL Injections and other issues.
+$statement = $db->query("select * from test limit 1");
 $statement->execute();
 $res = $statement->fetchall();
 dump($res);
